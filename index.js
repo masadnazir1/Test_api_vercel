b2468059f72dadbc39eaa42150be3626a4c6b5dc
noetic-asad <asad@noeticworld.com>
2024-06-25 17:50:22 +0500

    added the Vercel json file with express

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// index.js
+const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000; // Use port from environment variable or 3000 by default
-
-// Sample product data array
-const products = [
-  { id: 1, name: 'Product 1', price: 10.99 },
-  { id: 2, name: 'Product 2', price: 19.99 },
-  { id: 3, name: 'Product 3', price: 5.99 },
-];
-
-// Define a route to get all products
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
